863b6e3fd72173bd2190e66589384bc8b79baeda
firman2528 <firman.yuspriyadi@gmail.com>
2019-09-04 14:51:52 +0700

    perbaikan hari kedua minggu-3

--- minggu-3/hari-kedua/latihan/flask/app/__init__.py
+++ minggu-3/hari-kedua/latihan/flask/app/__init__.py
@@ -1,15 +1,7 @@
 from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
-from app import views, app
-from app import views
-from app import admin_views
+
 
 app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app,db)
 
-from app import routes, models
+from app import views
--- minggu-3/hari-kedua/latihan/flask/app/config.py
+++ minggu-3/hari-kedua/latihan/flask/app/config.py
@@ -6,4 +6,15 @@ class Config(object) :
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqllite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+
+class DevelopmentConfig(object) :
+    DEBUG=True
+    SQLALCHEMY_ECHO = True
+
+class ProductionConfig(object) :
+    DEBUG = False
+
+app_config={
+    'development' : DevelopmentConfig,
+    'production' : ProductionConfig
+}
